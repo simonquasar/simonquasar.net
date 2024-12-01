d27a38f12c1b55970297bcc22c1c6475c35a15d5
Simon Pilati <simonquasar@gmail.com>
2024-12-01 13:13:06 +0100

    shuppan

---
+++ shuppan/index.php
@@ -0,0 +1,258 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <title>La nascita dell’editoria moderna in Giappone 1853–1890</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords" content="Simon Pilati, editoria giapponese, nascita editoria moderna, libro Giappone, evoluzione stampa giapponese, Fukuzawa Yukichi, bunmei kaika, kisha club, editoria, ricerche storiche, storia giapponese, zasshi, oranda fūsetsugaki, Giappone, cultura, Meiji, tipografia, shinbun, Fukuchi Genichirō, Edoardo Chiossone, Ōshinbun, Ukiyo-e, Bakumatsu, Letteratura giapponese, Critica letteraria, Stampa, Meirokusha, Politica editoriale, Giornalismo, Libertà di stampa, Tradizione letteraria, Yellow Papers, Hakubunkan, Maejima Hisoka, Mori Arinori, yōsho shirabesho, Tokutomi Sohō, Kokumin no Tomo, Mainichi Shimbun, amakudari, goyō shinbun, fuhen futō, pamphlet, Daijō-kan Nisshi, Shinbunshi, diritto d'autore, copyright, Nagasaki Shipping List and Advertiser, The Japan Herald, Nihon Boheki Shinbun, The Far East, Tōkyō Nichi Nichi Shinbun, John Reddie Black, Horatio Nelson Lay, Matthew Calbraith Perry, Yanagawa Shunsan, Itō Hirobumi">
+    <meta name="description" content="L’evoluzione dell'editoria moderna e stampa giornalistica in Giappone tra il 1853 e il 1890 attraverso un'indagine approfondita delle sviluppi tecnologici, sociali e politici dell'epoca."">
+    <meta property="og:description" content="Esplora l’evoluzione dell'editoria moderna e stampa giornalistica in Giappone tra il 1853 e il 1890 attraverso un'indagine approfondita delle sviluppi tecnologici, sociali e politici dell'epoca.">
+    <meta property="og:type" content="book">
+    <meta property="og:title" content="La nascita dell’editoria moderna in Giappone 1853–1890 - Libro">
+    <meta property="og:url" content="https://shuppan.simonquasar.net">
+    <meta property="og:image" content="https://shuppan.simonquasar.net/nemg.webp">
+    <meta property="og:image:width" content="1200">
+    <meta property="og:image:height" content="630">
+    <meta property="profile:first_name" content="Simon">
+    <meta property="profile:last_name" content="Pilati">
+    <meta name="theme-color" content="#ff3824">
+    <meta name="msapplication-TileColor" content="#ff3824">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    <link rel="apple-touch-icon" sizes="180x180" href="https://simonquasar.net/apple-touch-icon.png?v=1.1">
+    <link rel="icon" type="image/png" sizes="32x32" href="https://simonquasar.net/favicon-32x32.png?v=1.1">
+    <link rel="icon" type="image/png" sizes="16x16" href="https://simonquasar.net/favicon-16x16.png?v=1.1">
+    <link rel="manifest" href="https://simonquasar.net/site.webmanifest?v=1.1">
+    <link rel="shortcut icon" href="https://simonquasar.net/favicon.ico?v=1.1">
+    <link rel="image_src" href="https://simonquasar.net/images/Q.png">
+    <link rel="stylesheet" href="https://simonquasar.net/2023.css"; type="text/css" media="screen, projection">
+    <script src="https://code.jquery.com/jquery-latest.min.js"; type="text/javascript"></script>
+	 <style>
+        ul {
+            list-style-type: none;
+            padding-left: 0;
+        }
+        li {
+            padding-left: 10px;
+        }
+        li ul {
+            padding-left: 10px;
+        }
+        li ul li {
+            padding-left: 10px;
+        }
+    </style>
+</head>
+<body>
+    <div id="bt"></div>
+    <div id="br"></div>
+    <div id="bl"></div>
+    <div id="bb"></div>
+
+    <div id="wrap" style="min-width:200px">
+        <div id="center">
+            <div class="header">
+                <a href="https://simonquasar.net"; target="_self" title="simonquasar">
+                    <img class="header-img" id="qh" src="https://simonquasar.net/q18.png"; alt="simonquasar">
+                </a>
+                <b><i>simonquasar</i></b><br>
+                <i style="opacity: 0.7">simon pilati</i><br>
+                <i style="opacity: 0.7"><small>サイモン　ピラティ</small></i><br>
+            </div>
+        </div>
+
+        <div id="right" style="max-width: 100%">
+            <div class="section" style="height:auto;" id="nemg">
+                <h1 id="contact-header" style="margin: 20px 0; color:var(--branch-identity);">明治の出版</h1>
+				<p style="line-height: 1.3em;"><i>- “Why?” [the merchant] asked. “I’ve got it [the yesterday newspaper] - what more do I want?” <br>  
+				- “Yes, you have one day’s issue, and it comes out every day.”   <br>
+				- “So I understand,” he replied, “but having it already, why should I take it everyday?” <br>  
+				And all the clerks laughed, thinking it an excellent stroke of wit, no doubt. </i>
+				</p>
+                <br>
+                <h3 style="font-size: 1.4em;"><i>La nascita dell’editoria moderna in Giappone <span style="font-weight:lighter">1853–1890</span></i></h3>
+                <br><i>Simon Pilati, 2024</i>
+				<br>19,20€ - 154 pagg. a colori<br>
+				<small>ISBN 9781446129609<br>
+                © Giugno 2024, Italia</small>
+				
+				<br>
+				<br><a href="https://bit.ly/EditoriaMeiji_LibroIT"; target="_blank" alt="La nascita dell’editoria moderna in Giappone" style="color:var(--branch-print);">
+                    <img src="https://shuppan.simonquasar.net/nemg.webp"; alt="Cover: La nascita dell'editoria moderna in Giappone" style="width: 30%; min-width:220px;float: left; margin: 20px;"></a><br>
+
+                <p>
+                    Questo libro esamina in dettaglio <b>l’evoluzione dell’editoria e la nascita della stampa giornalistica giapponese dagli anni pre-Meiji fino alla fine del secolo XIX.</b>
+					<br><br>Assieme alle numerose controversie legali, avventure commerciali e ai tumulti politici che caratterizzavano l’epoca, vengono tracciati i ruoli importanti di figure come Fukuzawa Yukichi, Fukuchi Gen’ichirō o Edoardo Chiossone. 
+					<br>Esplorando le relazioni tra editori, autori, Stato e opinione pubblica, il saggio delinea gli sviluppi tecnologici, il ruolo degli <i>oranda fūsetsugaki</i>, la nascita dei primi <i>shinbun</i> e degli <i>zasshi</i> fino all’avvento dei <i>kisha club</i>.
+					<br><b>Un’analisi inedita per comprendere le fondamenta dell’industria editoriale giapponese moderna</b>. 
+                </p>
+				
+                <p>
+                    L’<b>introduzione</b> fornisce il contesto storico necessario per comprendere il panorama del Giappone prima del 1853, nonché i rapporti degli eventi internazionali riportati dai mercanti olandesi, ossia gli <i>oranda fūsetsugaki</i>.
+                </p>
+
+                <p>
+                    Nella parte "<b>Infrastruttura Editoriale</b> 1853–1875", si esamina l'arrivo di un nuovo mondo con l'apertura dei porti giapponesi e la conseguente inondazione di informazioni e idee stranere. Qui si approfondisce il ruolo di figure come Yanagawa Shunsan e le pubblicazioni dello <i>yōsho shirabesho</i>, il primo giornale in lingua autoctona e l'influenza di Fukuzawa Yukichi sul <i>bunmei kaika</i>.
+                </p>
+
+                <p>
+                    In  "<b>Sviluppo Tecnologico</b> 1590–1900" si esplorano le sfide tipografiche, la distribuzione e la creazione del sistema postale lungo l'arcipelago. Inoltre, discuteremo delle tecniche di abbonamento, delle questioni legate alla carta, e degli impatti delle crisi politiche, come la ribellione di Satsuma, sull'editoria.
+                </p>
+
+                <p>
+                    Nella sezione "<b>Stato ed Editoria</b> 1868–1890", il saggio descrive i rapporti tra politica ed editoria, il movimento per i diritti del popolo e la nascita dei <i>kisha club</i>.
+                </p>
+ 
+                <p>
+                    In "<b>Cultura Letteraria</b> 1870-1890" si delinea la formazione dell'opinione pubblica e della cultura "moderna", l'ascesa delle <i>zasshi</i> (riviste), dei Yellow Papers, e l'apparizione di cartelli dell'informazione.
+                </p>
+
+                <p>
+                    In <b>Appendice</b>, infine, ulteriori dettagli e risorse preziose, inclusi <b>elenchi di pubblicazioni</b>, eventi storici, <b>legislazioni</b> e <b>dati statistici</b>, che offrono un quadro completo e un supporto empirico e statistico alle analisi presentate.
+                </p>
+ 
+                
+				<br><a href="#index"; target="_self" alt="Index" style="color:var(--branch-releases);"><b>Indice</b></a> - <a href="https://cloud.simonquasar.net/index.php/s/NEMG_anteprima_0724"; target="_blank" alt="Preview" style="color:var(--branch-webdesign);"><b>Vedi l'anteprima</b></a> - <a href="https://bit.ly/EditoriaMeiji_LibroIT"; target="_blank" alt="La nascita dell’editoria moderna in Giappone" style="color:var(--branch-print);"><b>Acquista</b>
+                </a><br>
+                19,20€ - 154 pagg. a colori<br>
+                <small>ISBN 9781446129609<br>
+                Giugno 2024, Italia<br>
+                © Pubblicato da Simon Pilati. Tutti i diritti riservati.</small>
+            </div>
+
+            <hr>
+
+            <div class="section" style="height:auto;" id="index">
+                <h3>Indice</h3><br>
+                <p>
+				  <ul>
+					  <li>Prefazione</li>
+					  <li>Introduzione</li>
+					  <li>Nota sulla traslitterazione</li>
+					  <li>
+						<b>Pre-Meiji (–1853)</b>
+						<ul>
+						  <li>Ultime immagini dell’era Tokugawa</li>
+						  <li>1.1 Le immagini del mondo fluttuante</li>
+						  <li>1.2 Il <i>bakumatsu</i> e gli <i>oranda fūsetsugaki</i></li>
+						</ul>
+					  </li>
+					  <li>
+						<b>Infrastruttura Editoriale (1853–1875)</b>
+						<ul>
+						  <li>2. Arrivo di un nuovo mondo</li>
+						  <li>2.1 Notizie nell’arcipelago</li>
+						  <li>Yanagawa Shunsan e pubblicazioni dello <i>yōsho shirabesho</i></li>
+						  <li>Primo giornale in lingua giapponese</li>
+						  <li>Fukuzawa Yukichi e il <i>bunmei kaika</i></li>
+						  <li>2.2 La guerra dell’anno del drago</li>
+						  <li>Prima ondata di pubblicazioni</li>
+						  <li>2.3 La corsa all’oro dell’editoria moderna</li>
+						  <li>Patrocinio statale: <i>amakudari</i> e <i>goyō shinbun</i></li>
+						  <li>Mori Arinori e la <i>meirokusha</i></li>
+						  <li><i>Ōshinbun</i> e <i>koshinbun</i></li>
+						  <li>Nuovi formati di lettura</li>
+						  <li>2.4 Primo boom editoriale - <i>shinbun</i></li>
+						</ul>
+					  </li>
+					  <li>
+						<b>Sviluppo Tecnologico (1590–1900)</b>
+						<ul>
+						  <li>3. 100,000 caratteri mobili e altre sfide tipografiche</li>
+						  <li>3.1 La distribuzione del <i>bunmei kaika</i></li>
+						  <li>Maejima Hisoka, il "padre" del sistema postale</li>
+						  <li>Abbonamenti e reti di lettori</li>
+						  <li>3.2 Caratteri mobili</li>
+						  <li>3.3 Questioni di carta</li>
+						  <li>3.4 Il fragore delle crisi</li>
+						  <li>La ribellione di Satsuma</li>
+						</ul>
+					  </li>
+					  <li>
+						<b>Stato ed Editoria (1868–1890)</b>
+						<ul>
+						  <li>4. L’invenzione del quarto potere</li>
+						  <li>Economia sviluppista</li>
+						  <li>4.1 Stato vs. editori</li>
+						  <li>Movimento per i Diritti del Popolo</li>
+						  <li>Fukuchi Genichirō e il <i>fuhen futō</i></li>
+						  <li>Nuovi partiti per un nuovo gabinetto</li>
+						  <li>4.2 I <i>kisha club</i>: fonti di controllo o censura?</li>
+						  <li>Accesso alla Dieta e nuovi privilegi</li>
+						  <li>4.3 Diritti d’autore e ricatti globali</li>
+						  <li>Statuti sulla Stampa (<i>jōrei</i>) del 1887</li>
+						  <li>Tabella delle legislazioni dal 1869 al 1887</li>
+						  <li>Convenzione di Berna</li>
+						</ul>
+					  </li>
+					  <li>
+						<b>Cultura Letteraria (1870–1890)</b>
+						<ul>
+						  <li>5. Avviamento di una cultura letteraria</li>
+						  <li>5.1 La formazione dell’opinione pubblica</li>
+						  <li>Principi morali</li>
+						  <li>Biblioteche pubbliche</li>
+						  <li>5.2 L’ascesa di <i>zasshi</i> e Yellow Papers</li>
+						  <li>Le riviste</li>
+						  <li>Yellow Papers</li>
+						  <li><i>Hakubunkan</i> e cartelli dell’informazione</li>
+						  <li>5.3 L’avvento della critica e dei gusti letterari moderni</li>
+						  <li>Tokutomi Sohō e il <i>Kokumin no Tomo</i></li>
+						</ul>
+					  </li>
+					  <li>Epilogo</li>
+					  <li>
+						<b>Appendice</b>
+						<ul>
+						  <li>I - Lista pubblicazioni moderne pre-Meiji</li>
+						  <li>II - Lista maggiori pubblicazioni 1868–1890</li>
+						  <li>III - Avvenimenti, leggi e pubblicazioni 1851–1899</li>
+						  <li>IV - Tiratura giornaliera 1875–1907 e 1885–1907</li>
+						  <li>V - Numero periodici registrati 1885–1905</li>
+						  <li>VI - Azioni giudiziarie vs. editoria 1876–1899</li>
+						</ul>
+					  </li>
+					  <li>Glossario</li>
+					  <li>Indice dei nomi</li>
+					  <li>Indice delle pubblicazioni</li>
+					  <li>Bibliografia</li>
+					  <li>Sitografia</li>
+					  <li>Fonti grafiche</li>
+					</ul>
+					<br><a href="https://cloud.simonquasar.net/index.php/s/NEMG_anteprima_0724"; target="_blank" alt="Preview" style="color:var(--branch-webdesign);"><b>Vedi l'anteprima</b></a> - <a href="https://bit.ly/EditoriaMeiji_LibroIT"; target="_blank" alt="La nascita dell’editoria moderna in Giappone" style="color:var(--branch-print);"><b>Acquista</b></a>
+					<br>19,20€ - 154 pagg. a colori
+					<br>
+					<br><small>ISBN 9781446129609
+					<br>Giugno 2024, Italia
+					<br>© Pubblicato da Simon Pilati. Tutti i diritti riservati.</small>
+                </p>
+            </div>
+
+            <hr>
+			
+            <div class="section"  style="height:auto;" id="contact">
+                <h3>L'autore</h3><br>
+                <p>Nato nel <b>1985</b>, Simon Pilati ha studiato lingue e letterature straniere moderne presso l'Università di Bologna.
+				<br>Da oltre dodici anni si occupa di <b>servizi per l'editoria</b> e piattaforme culturali tra Roma, Berlino e Palermo.
+				<br>Attualmente vive sulle <b>Dolomiti</b>.
+				<br>Domande, correzioni e informazioni a mezzo email > <a href="mailto:s@simonquasar.net"; target="_blank" title="email" style="color:var(--branch-print);">s@simonquasar.net</a>
+                </p>
+            </div>
+
+            <hr>
+
+            <div class="section"  style="height:auto;" id="copyright">
+                <div class="copyright" style="opacity:0.8">
+                    <b>© 2012-<?php echo date("Y"); ?>, Simon Pilati - All rights reserved. All materials on these pages are copyrighted</b>, unless otherwise indicated. 
+                    No part of these pages, either text or images may be used for any purpose other than personal use, unless explicit authorization is given. Therefore reproduction, modification, storage in a retrieval system or retransmission, in any form or by any means – electronic, mechanical or otherwise, for reasons other than personal use, is strictly prohibited without prior written permission.
+                </div>
+                <div class="copyright" style="opacity:0.7">
+                    <u><b>GDPR</b>/EU: No tracking scripts or privacy spies to declare.</u> :)
+                </div>
+            </div>
+			
+
+        </div>
+    </div>
+</body>
+</html>
